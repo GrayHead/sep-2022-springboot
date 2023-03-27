3504cf0b7af3acbf38e142ce9bf873f5c3291d4c
GrayHead <ss.zhuravlov@gmail.com>
2023-03-27 19:47:48 +0300

    init

---
+++ src/main/java/ua/com/owu/sep2022springboot/Sep2022SpringbootApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.sep2022springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep2022SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep2022SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/owu/sep2022springboot/Sep2022SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.sep2022springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep2022SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
