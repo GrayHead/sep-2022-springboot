3950a4dedb92e22d72e30cfba6bb74bd862b5063
GrayHead <ss.zhuravlov@gmail.com>
2023-03-28 00:55:03 +0300

    dlete

---
+++ src/main/java/ua/com/owu/sep2022springboot/actions/Actions.java
@@ -0,0 +1,21 @@
+package ua.com.owu.sep2022springboot.actions;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import ua.com.owu.sep2022springboot.dao.UserDAO;
+import ua.com.owu.sep2022springboot.models.User;
+
+import java.util.List;
+
+
+public class Actions {
+
+    public  class GetActions {
+        public List<User> getUsers() {
+
+            return null;
+
+        }
+
+    }
+}
